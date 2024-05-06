daa1abbda13b5b92c9c3c0ba9be7929fff1b3680
Safelg Preciado <Safelgno1@gmail.com>
2024-05-05 18:23:52 -0700

    feat: add more functionality for blog controller

--- server/main.py
+++ server/main.py
@@ -49,6 +49,44 @@ def create_blog():
 
     return jsonify({'message': 'Blog created'}), 201
 
+@app.route("/api/blogs", methods=["GET"])
+def get_blogs():
+    blogs = Blog.query.all()
+    return jsonify([{'title': blog.title, 'url': blog.url, 'author': blog.author, 'likes': blog.likes, 'user': blog.user.to_json()} for blog in blogs])
+
+@app.route("/api/users/<int:user_id>/blogs", methods=["GET"])
+def get_user_blogs(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify([blog.to_json() for blog in user.blogs])
+
+@app.route("/api/blogs/<int:blog_id>", methods=["DELETE"])
+def delete_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if not blog:
+        return jsonify({'message': 'Blog not found'}), 404
+    db.session.delete(blog)
+    db.session.commit()
+    return jsonify({'message': 'Blog deleted'}), 200
+
+@app.route("/api/blogs/<int:blog_id>", methods=["PUT"])
+def update_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if not blog:
+        return jsonify({'message': 'Blog not found'}), 404
+    title = request.json.get('title')
+    author = request.json.get('author')
+    url = request.json.get('url')
+    if title is not None:
+        blog.title = title
+    if author is not None:
+        blog.author = author
+    if url is not None:
+        blog.url = url
+    db.session.commit()
+    return jsonify({'message': 'Blog updated'}), 200
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
